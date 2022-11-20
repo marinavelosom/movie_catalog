a6d70516717e0528ba11fc509551a05ebca8f6a3
Wallison José <its.wallisonjose@gmail.com>
2022-11-20 18:51:07 -0300

    final changes 2

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,15 @@
 class Comment < ApplicationRecord
+  after_update :create_log, if: Proc.new {|comment|comment.aprove} 
+  
   belongs_to :movie
+
+  has_one :log, dependent: :destroy
 
   validates :content, :movie, presence: true
   scope :not_aproved, -> {where(aprove: false)}
   scope :aproved, -> { where(aprove: true) }
+
+  def create_log
+    Log.create(comment:self)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_20_184144) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_20_212329) do
   create_table "actors", force: :cascade do |t|
     t.string "name"
     t.integer "birth_year"
@@ -37,6 +37,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_20_184144) do
     t.index ["movie_id"], name: "index_comments_on_movie_id"
   end
 
+  create_table "logs", force: :cascade do |t|
+    t.string "index"
+    t.integer "comment_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["comment_id"], name: "index_logs_on_comment_id"
+  end
+
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.integer "release_year"
@@ -47,4 +55,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_20_184144) do
   add_foreign_key "casts", "actors"
   add_foreign_key "casts", "movies"
   add_foreign_key "comments", "movies"
+  add_foreign_key "logs", "comments"
 end
