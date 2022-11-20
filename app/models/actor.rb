38af0b78eb9bf62fd0c50261e503afb94f61fb9e
Gustavo <gustavopchaves@hotmail.com>
2022-11-20 09:23:39 -0300

    ✨ Adicionada validação de ano de nascimento em ator

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,5 @@
 class Actor < ApplicationRecord
   validates :name, :birth_year, :email, presence: true
   validates :email, uniqueness: true
+  validates :birth_year, comparison: { greater_than: 0, less_than_or_equal_to: Date.today.year }
 end
