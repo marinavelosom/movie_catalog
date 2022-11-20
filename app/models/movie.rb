2bf4f3296b6a4bfb107d193f515e389648559004
Wallison José <its.wallisonjose@gmail.com>
2022-11-20 00:23:14 -0300

    Validações p/ releaseDate e title

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,7 @@
 class Movie < ApplicationRecord
+   
+    validates :title, presence: true
+    validates :title, uniqueness: { case_sensitive: false }
+    validates :releaseDate, presence: true
+
 end
