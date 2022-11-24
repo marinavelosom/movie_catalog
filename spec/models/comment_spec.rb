248e9b57e7b58dfd75a4d67819794000935d36df
marinavelosom <engmarinamelo@gmail.com>
2022-11-24 10:44:30 -0300

    fix comments tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  context 'validations' do
-    actor = Actor.create(name: "some_name", birth_year: 2001, email: "actor@test.com")
-    movie = Movie.create(id: 5, title: "some_title", release_year: 2021, actors: [actor])
+  actor = Actor.create(name: "some_name", birth_year: 2001, email: "actor@test.com")
+  movie = Movie.create(id: 5, title: "some_title", release_year: 2021, actors: [actor])
 
+  context 'validations' do
     it 'is valid with valid attibutes' do
       comment = Comment.new(content: "Primeiro comentário", movie_id: movie.id)
       expect(comment).to be_valid
@@ -17,16 +17,6 @@ RSpec.describe Comment, type: :model do
 
     it 'not is valid without movie' do
       comment = Comment.new(content: "Primeiro comentário") #ou seja a validação do model está funcionando
-      expect(comment).to_not be_valid
-    end
-
-    it 'not valid if text blank' do 
-      comment = Comment.new(content: nil) 
-      expect(comment).to_not be_valid
-    end
-    
-    it 'not valid if movie blank' do 
-      comment = Comment.new(movie_id: nil) 
       expect(comment).to_not be_valid
     end
   end
